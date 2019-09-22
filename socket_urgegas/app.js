d6aace0a241a86a04276b6d9169d1c4affcf3d4a
Sergio Sosa <sergiososa@MacBook-Air-de-Sergio.local>
2019-09-22 15:57:44 -0500

    Se agregaron comentarios para el socket

--- socket_urgegas/app.js
+++ socket_urgegas/app.js
@@ -1,6 +1,7 @@
 "use strict";
 var express = require("express");
 var bodyParser = require("body-parser");
+//Inicializacion de npm para poner autenticacion al socket por medio de tokens
 var jwtAuth = require("socketio-jwt-auth");
 
 var app = express();
@@ -12,6 +13,8 @@ let loginRoutes = require("./routes/LoginRoutes");
 //Here are middlewares
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//Middleware en donde se autentica el socket, si contiene el token procede a conectarse al socket, en caso de no tener token no hace nada
 io.use(
   jwtAuth.authenticate(
     {
@@ -36,15 +39,21 @@ app.use((req, res, next) => {
 
   next();
 });
+
+//Objeto que funciona para almacenar a los clientes que se estan conectando y de esa manera mandar la informacion al dashboard a pintar
 var clients = {};
 //Socket
 
+//Metodo en donde se indica que el usuario se conecto al socket
 io.on("connection", function(socket) {
   console.log("El nodo: " + socket.handshake.address + " se ha conectado");
   socket.on("coordenites", function(data) {
+    //Se almacena el id del socket como nombre de la propiedad del objeto cliente y se agregan las coordenadas
     clients[socket["id"]] = data;
     //io.sockets.emit("clients-coordinates", messages);
   });
+
+  //Metodo que nos indica cuando se desconecta un dispositivo
   socket.on("disconnect", function() {
     console.log("Dispositivo Desconectado");
     delete clients[socket["id"]];
